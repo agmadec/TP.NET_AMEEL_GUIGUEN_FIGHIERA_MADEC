04819a16f5371ebd715e0d8ed89ffa581669b982
agmadec <114055198+agmadec@users.noreply.github.com>
2023-02-01 15:29:12 +0100

    tentative titre+info

--- WPF.Reader/Service/LibraryService.cs
+++ WPF.Reader/Service/LibraryService.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.ObjectModel;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using WPF.Reader.Model;
 
 namespace WPF.Reader.Service
@@ -10,10 +11,15 @@ namespace WPF.Reader.Service
         // Donc pas de LibraryService.Instance.Books = ...
         // mais plutot LibraryService.Instance.Books.Add(...)
         // ou LibraryService.Instance.Books.Clear()
+        public ObservableCollection<Genre> Genres { get; set; } = new ObservableCollection<Genre>()
+        {
+           new Genre() { Name="Sciences-Fiction"},
+           new Genre(){ Name="Romantique"}
+        };
         public ObservableCollection<Book> Books { get; set; } = new ObservableCollection<Book>() {
-            new Book() { Title= "Super", Content= "Ta mere"},
-            new Book() { Title= "Super",Content= "Ta mere"},
-            new Book() { Title= "Super",Content= "Ta mere"}
+            new Book() { Title= "Super", Author= "Jojo", Price= 19, Genres= new List<Genre>(){Genres.[0]},  Content= "Ta mere"},
+            new Book() { Title= "Super", Author= "Jijo", Price= 19, Content= "Ta mere"},
+            new Book() { Title= "Super", Author= "Jiji", Price= 19, Content= "Ta mere"}
         };
 
         // C'est aussi ici que vous ajouterez les requête réseau pour récupérer les livres depuis le web service que vous avez fait
