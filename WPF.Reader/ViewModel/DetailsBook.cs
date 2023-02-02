41643e21f7de395f12794b8e2fd2ad1265ea5bfb
Dr. Droïde <u.fighiera@free.fr>
2023-02-02 08:59:56 +0100

    Upgrading DetailsBook

--- WPF.Reader/ViewModel/DetailsBook.cs
+++ WPF.Reader/ViewModel/DetailsBook.cs
@@ -1,5 +1,6 @@
 ﻿using CommunityToolkit.Mvvm.DependencyInjection;
 using System.ComponentModel;
+using System.Text.RegularExpressions;
 using System.Windows.Input;
 using System.Windows.Navigation;
 using WPF.Reader.Model;
@@ -14,10 +15,24 @@ namespace WPF.Reader.ViewModel
 
         // n'oublier pas faire de faire le binding dans DetailsBook.xaml !!!!
         public Book CurrentBook { get; init; }
+        public int NombreDeMots { get; init; }
+        public string TempsDeLecture { get; init; }
+        public int TempsDeLectureMinute { get; init; }
+        public int TempsDeLectureSeconde { get; init; }
+        public int VitesseDeLecturre { get; init; } = 150;
 
         public DetailsBook(Book book)
         {
             CurrentBook = book;
+            string sansBalises = Regex.Replace(book.Content, "<[^>]*>", string.Empty);
+            string[] mots = sansBalises.Split(' ');
+            NombreDeMots = mots.Length;
+            float minutes = (float)NombreDeMots / VitesseDeLecturre;
+            int secondes = (int)(minutes*60);
+            TempsDeLectureMinute = secondes / 60;
+            TempsDeLectureSeconde = secondes % 60;
+            TempsDeLecture = string.Format("~{0:0} : {1:00}", TempsDeLectureMinute, TempsDeLectureSeconde);
+
         }
     }
 
